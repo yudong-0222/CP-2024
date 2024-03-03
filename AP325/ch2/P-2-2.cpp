8363115fbc5500554e337da5be0d1cf5584fbc57
yudong-0222 <yuchi9716@gmail.com>
2024-03-04 01:55:27 +0800

    P-2-2.cpp

--- AP325/ch2/P-2-2.cpp
+++ AP325/ch2/P-2-2.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
-#define read(type) readInt<type>() // Fast read
 #define int long long
 #define endl "\n"
-#define pb push_back
-#define eb emplace_back
-#define mk make_pair
-#define pii pair<int, int>
-#define F first
-#define S second
-#define vi vector<int>
-#define all(x) (x).begin(), (x).end()
-#define umap unordered_map
-#define uset unordered_set
-#define MOD 1e9 + 7
-#define imax INT_MAX
-#define imin INT_MIN
-#define exp 1e9
-#define sz(x) (int((x).size()))
-#define N 100010
 using namespace std;
 
 signed main(void) {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
     int n; cin >> n;
-    int a[N], b[N];
+    set<int> st;
+    vector<int> p(100010);
 
-    for(int i = 0; i < n; ++i)
-        cin >> a[i];
+    for(int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        p[i] = x;
+        st.emplace(x);
+    }
+
+    vector<int> co(st.begin(), st.end());
     
+    for(int i = 0; i < n; ++i) {
+        auto it = lower_bound(co.begin(), co.end(), p[i]);
+        p[i] = distance(co.begin(), it);
+    }
 
-
+    // for(int i = 0; i < n; ++i) {
+    //     auto it = st.lower_bound(p[i]);
+    //     p[i] = distance(st.begin(), it);
+    // }
+    
+    for(int i = 0; i < n-1; ++i) cout << p[i] << " ";
+    cout << p[n-1] << endl;
 }
